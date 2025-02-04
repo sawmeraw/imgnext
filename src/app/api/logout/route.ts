13f967b7c38709baf7c9299d31b11a9d05c8d8cf
Sameer Panday <pandeysameer96@gmail.com>
2025-02-04 23:36:16 +1100

    logout route await cookie bug fixed

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -2,9 +2,9 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 
-export async function GET(req: Request){
+export async function GET(req: Request) {
 
-    const cookieStore = cookies()
+    const cookieStore = await cookies()
     cookieStore.delete('x-authenticated')
 
     return NextResponse.redirect(new URL('/login', req.url))
