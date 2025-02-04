607a4dd0398ec71399557a7c727b3219b44456ef
Sameer Panday <pandeysameer96@gmail.com>
2025-02-04 23:33:01 +1100

    cookies await bug fixed

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -5,27 +5,27 @@ import { NextResponse } from "next/server"
 
 const validPassword = process.env.LOGIN_PASSWORD
 
-export async function POST(req: Request){
+export async function POST(req: Request) {
 
     const data: LoginRequest = await req.json()
-    
-    if(!data.password || data.password == ""){
-        return NextResponse.json({message: "Empty Request!"}, {status: 401})
+
+    if (!data.password || data.password == "") {
+        return NextResponse.json({ message: "Empty Request!" }, { status: 401 })
     }
 
-    if (data.password === validPassword){
-        const cookieStore = cookies()
+    if (data.password === validPassword) {
+        const cookieStore = await cookies()
         cookieStore.set('x-authenticated', 'true', {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== "development" ,
-            maxAge: 2*60*60,
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: 2 * 60 * 60,
             path: '/',
             sameSite: "lax"
         })
-        const res =  NextResponse.json({message: "Lets go."}, {status: 200})
+        const res = NextResponse.json({ message: "Lets go." }, { status: 200 })
         return res
-    } else{
-        return NextResponse.json({message: "Wrong password!"}, {status: 401})
+    } else {
+        return NextResponse.json({ message: "Wrong password!" }, { status: 401 })
     }
 
 }
