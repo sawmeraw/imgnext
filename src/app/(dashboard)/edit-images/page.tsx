3ef8cfc755a86be7a7956d4271f76b208cf4ee63
Sameer Panday <pandeysameer96@gmail.com>
2025-02-17 17:55:04 +1100

    maintains the 1:1 aspect ratio

--- src/app/(dashboard)/edit-images/page.tsx
+++ src/app/(dashboard)/edit-images/page.tsx
@@ -4,8 +4,8 @@ export default function EditImagesPage() {
     return (
         <>
             <main className="bg-white rounded px-4 py-2 text-black w-full min-h-[700px]">
-                <h2 className="font-semibold text-2xl mt-4">Remove Whitespace</h2>
-                <p className="text-sm font-semibold ">100px of max whitespace on all sides.</p>
+                <h2 className="font-semibold text-2xl mt-4">Crop and Resize Image </h2>
+                <p className="text-sm font-semibold ">This will crop whitespace and maintain 1:1 aspect ratio.</p>
                 <section className="w-full mt-4">
 
                     <ImageUploadForm />
